366ce2ba44eb0408db42796c63e726435f298641
Mohd Saddam <54094219+saddam369@users.noreply.github.com>
2026-05-13 23:28:12 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int i = 0;
+        int j = 1;
+        while (j < prices.length) {
+            if (prices[i] < prices[j]) {
+                max = Math.max(prices[j] - prices[i], max);
+            } else {
+                i = j;
+            }
+            j++;
+        }
+
+        return max;
+    }
+}
