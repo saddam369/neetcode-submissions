4a1203e011d1c38ad790c0a23604970470ab66f2
Mohd Saddam <54094219+saddam369@users.noreply.github.com>
2026-05-15 19:12:10 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        for( char c : s.toCharArray()){
+            if(map.containsKey(c)){
+               if(st.peek() == map.get(c)){
+                st.pop();
+               }else{
+                return false;
+               }
+            }else{
+                st.push(c);
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
